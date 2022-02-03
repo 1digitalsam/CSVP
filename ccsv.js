32559a9831a112eb3be8bdabce7be676bd5c92a6
1digitalsam <1digitalsam@gmail.com>
2022-02-02 19:37:57 -0500

    firstcommit

--- ccsv.js
+++ ccsv.js
@@ -1,5 +1,9 @@
 const {load} = require('csv-load-sync')
 const csv = load('coin_Bitcoin.csv')
+const csvd = load('coin_Dogecoin.csv')
+const csve = load('coin_Ethereum.csv')
 
 
-console.log(csv)
+console.log(csv[0])
+console.log(csvd[0])
+console.log(csve[0])
